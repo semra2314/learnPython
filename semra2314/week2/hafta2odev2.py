2133248b8c1fc750beed6dbd5a53b495b6255352
Semra <semrayaslann23@gmail.com>
2025-03-18 14:04:15 +0300

    Update hafta2odev2.py

---
+++ semra2314/week2/hafta2odev2.py
@@ -0,0 +1,15 @@
+''' 2 - ascıı art denen bir yazılım içerisinde sanat var onu yapmanızı isteyeceğiz internetten yardım alabilirsiniz 
+elmas yapmanızı istiyoruz
+
+Örnek: *
+      ***
+     *****
+    *******
+   *********
+    *******
+     *****
+      ***
+       * 
+şeklinde
+********************************
+'''
